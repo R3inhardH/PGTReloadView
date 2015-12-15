c98451075746a6e4fd076c2ac5eb12d0ed2631db
Reinhard Heimhuber <reinhard@vuframelab.com>
2015-12-15 21:40:48 +0100

    Initial commit

---
+++ PGTReloadView/PGTReloadView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PGTReloadView
+//
+//  Created by Reinhard on 15/12/15.
+//  Copyright © 2015 PGIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
