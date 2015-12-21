0fc0f4c4e0d7fe876d61b5a325d0619f5b0e48ae
Reinhard Heimhuber <reinhard@vuframelab.com>
2015-12-21 23:30:54 +0100

    Add trigger bounds and animations

--- PGTReloadView/PGTReloadView/ViewController.m
+++ PGTReloadView/PGTReloadView/ViewController.m
@@ -26,6 +26,7 @@ typedef enum {
 @implementation ViewController {
     CGSize _screenSize;
     eDirection _scrollingDirection;
+    BOOL _isReloadTriggered;
 }
 
 - (void)viewDidLoad {
@@ -37,6 +38,8 @@ typedef enum {
     
     _scrollingDirection = kDirection_none;
     _screenSize = [UIScreen mainScreen].bounds.size;
+    
+    _isReloadTriggered = NO;
 }
 
 
@@ -56,38 +59,51 @@ typedef enum {
 
 #pragma mark - UIScrollView Delegate
 
+#define kOffsetTrigger_TopBottom -80.0f
+#define kOffsetTrigger_BottomTop 80.0f
+#define kOffsetTrigger_LeftRight -80.0f
+#define kOffsetTrigger_RightLeft 80.0f
+
 
 - (void)scrollViewWillBeginDecelerating:(UIScrollView *)scrollView {
     _scrollView.scrollEnabled = NO;
+    if ([self validateScrollViewOffsetTrigger]) {
+        _isReloadTriggered = YES;
+    }
+}
+
+- (IBAction)testAction:(id)sender {
+    NSLog(@"test: %@", NSStringFromCGSize(_scrollView.contentSize));
 }
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
-    _scrollView.contentOffset = CGPointMake(0, 0);
-    _scrollView.contentSize = CGSizeMake(_screenSize.width + 1, _screenSize.height + 1);
-    _scrollingDirection = kDirection_none;
-    _scrollView.scrollEnabled = YES;
+    if (_isReloadTriggered) {
+        _scrollingDirection = kDirection_none;
+        _scrollView.scrollEnabled = YES;
+    }
+    else {
+        _scrollView.contentOffset = CGPointMake(0, 0);
+        _scrollingDirection = kDirection_none;
+        _scrollView.scrollEnabled = YES;
+    }
 }
 
 
 - (void)scrollViewDidScroll:(UIScrollView *)scrollView {
     if (_scrollingDirection == kDirection_none && !CGPointEqualToPoint(scrollView.contentOffset, CGPointZero)) {
-        _scrollingDirection = [self directionFromContentOffset:scrollView.contentOffset];
-        if (_scrollingDirection == kDirection_leftRight || _scrollingDirection == kDirection_rightLeft) {
-            _scrollView.contentSize = CGSizeMake(_screenSize.width + 1, _screenSize.height);
-        }
-        else {
-            _scrollView.contentSize = CGSizeMake(_screenSize.width, _screenSize.height + 1);
-        }
+        _scrollingDirection = [self determineScrollingDirectionFromContentOffset:scrollView.contentOffset];
     }
-    else {
-        if ([self isScrollingOutOfBorderLimit:_scrollingDirection]) {
-            _scrollView.contentOffset = CGPointMake(0, 0);
-        }
+    else if (_scrollingDirection != kDirection_none) {
+        scrollView.contentOffset = [self calculateValidOffset];
     }
 }
 
 
-- (eDirection)directionFromContentOffset:(CGPoint)offset {
+
+
+#pragma mark - Helper Methods
+
+- (eDirection)determineScrollingDirectionFromContentOffset:(CGPoint)offset {
     if (offset.x > 0 && offset.y > 0) {
         return offset.x >= offset.y ? kDirection_rightLeft : kDirection_bottomTop;
     }
@@ -110,26 +126,98 @@ typedef enum {
 }
 
 
-- (BOOL)isScrollingOutOfBorderLimit:(eDirection)activeScrollingDirection {
-    eDirection currentScrollingDirection = [self directionFromContentOffset:_scrollView.contentOffset];
-    if (activeScrollingDirection == currentScrollingDirection) {
-        return NO;
+
+#define kOffsetLimit_TopBottom -100.0f
+#define kOffsetLimit_BottomTop 100.0f
+#define kOffsetLimit_LeftRight -100.0f
+#define kOffsetLimit_RightLeft 100.0f
+
+
+- (CGPoint)calculateValidOffset {
+    CGPoint contentOffset = _scrollView.contentOffset;
+    switch (_scrollingDirection) {
+        case kDirection_topBottom:
+        {
+            if (contentOffset.y <= kOffsetLimit_TopBottom) {
+                return CGPointMake(0, kOffsetLimit_TopBottom);
+            }
+            else {
+                return CGPointMake(0, contentOffset.y < 0 ? contentOffset.y : 0);
+            }
+            break;
+        }
+            
+        case kDirection_bottomTop:
+        {
+            if (contentOffset.y >= kOffsetLimit_BottomTop) {
+                return CGPointMake(0, kOffsetLimit_BottomTop);
+            }
+            else {
+                return CGPointMake(0, contentOffset.y > 0 ? contentOffset.y : 0);
+            }
+            break;
+        }
+        case kDirection_leftRight:
+        {
+            if (contentOffset.x <= kOffsetLimit_LeftRight) {
+                return CGPointMake(kOffsetLimit_LeftRight, 0);
+            }
+            else {
+                return CGPointMake(contentOffset.x < 0 ? contentOffset.x : 0, 0);
+            }
+            break;
+        }
+        case kDirection_rightLeft:
+        {
+            if (contentOffset.x >= kOffsetLimit_RightLeft) {
+                return CGPointMake(kOffsetLimit_RightLeft, 0);
+            }
+            else {
+                return CGPointMake(contentOffset.x > 0 ? contentOffset.x : 0, 0);
+            }
+            break;
+        }
+        default:
+            return CGPointZero;
     }
-    
-    if (activeScrollingDirection == kDirection_bottomTop && currentScrollingDirection == kDirection_topBottom) {
-        return YES;
+}
+
+
+- (BOOL)validateScrollViewOffsetTrigger {
+    if (_scrollingDirection == kDirection_topBottom) {
+        if (_scrollView.contentOffset.y <= kOffsetTrigger_TopBottom) {
+            [UIView animateWithDuration:0.3f animations:^{
+                [_scrollView setContentOffset:CGPointMake(0, kOffsetTrigger_TopBottom) animated:NO];
+            }];
+            return YES;
+        }
     }
-    if (activeScrollingDirection == kDirection_topBottom && currentScrollingDirection == kDirection_bottomTop) {
-        return YES;
+    else if (_scrollingDirection == kDirection_bottomTop) {
+        if (_scrollView.contentOffset.y >= kOffsetTrigger_BottomTop) {
+            [UIView animateWithDuration:0.3f animations:^{
+                [_scrollView setContentOffset:CGPointMake(0, kOffsetTrigger_BottomTop) animated:NO];
+            }];
+            return YES;
+        }
     }
-    if (activeScrollingDirection == kDirection_rightLeft && currentScrollingDirection == kDirection_leftRight) {
-        return YES;
+    if (_scrollingDirection == kDirection_leftRight) {
+        if (_scrollView.contentOffset.x <= kOffsetTrigger_LeftRight) {
+            [UIView animateWithDuration:0.3f animations:^{
+                [_scrollView setContentOffset:CGPointMake(kOffsetTrigger_LeftRight, 0) animated:NO];
+            }];
+            return YES;
+        }
     }
-    if (activeScrollingDirection == kDirection_leftRight && currentScrollingDirection == kDirection_rightLeft) {
-        return YES;
+    else if (_scrollingDirection == kDirection_rightLeft) {
+        if (_scrollView.contentOffset.x >= kOffsetTrigger_RightLeft) {
+            [UIView animateWithDuration:0.3f animations:^{
+                [_scrollView setContentOffset:CGPointMake(kOffsetTrigger_RightLeft, 0) animated:NO];
+            }];
+            return YES;
+        }
     }
-    
     return NO;
 }
+
 
 @end
